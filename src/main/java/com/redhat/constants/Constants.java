9455caab6a6f67420a997edc13c2d2fa4c69989f
Marek Czernek <mczernek@redhat.com>
2019-06-27 14:18:26 +0200

    Provide more user friendly message when acronym is not found

--- src/main/java/com/redhat/constants/Constants.java
+++ src/main/java/com/redhat/constants/Constants.java
@@ -18,7 +18,8 @@ public class Constants {
     public static final String EXPLANATION_UPDATED = "Explanation updated. Thank you!";
     public static final String EXPLANATION_REMOVED = "Explanation removed. Thank you!";
     public static final String EXPLANATION_NOT_FOUND = "No such explanation with given Acronym. Are you sure the acronym is correct?";
-    public static final String ACRONYM_NOT_FOUND = "No such acronym found.";
+    public static final String ACRONYM_NOT_FOUND = "No such acronym found. Add it using the syntax `! acronym = explanation` (queries are case-insensitive), " + 
+            "or send `help` for more info";
 
     public static final String HELP_TEXT = "You are interacting with Acrobot. \n\n" +
             "Actions:\n" +
