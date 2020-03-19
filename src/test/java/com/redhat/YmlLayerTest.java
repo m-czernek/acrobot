1c87f786fe5eb7473d1a7947e269ccaf91a9ab17
Marek Czernek <mczernek@redhat.com>
2020-03-19 22:45:21 +0100

    Improve YmlDAL test

--- src/test/java/com/redhat/YmlLayerTest.java
+++ src/test/java/com/redhat/YmlLayerTest.java
@@ -6,21 +6,29 @@ import org.junit.Test;
 
 
 public class YmlLayerTest {
-
-    YamlDal dal = new YamlDal();
+    private YamlDal dal = new YamlDal();
     // Corner cases
     // Acro is a number
-    String numberAcro = "82576";
+    private static String numberAcro = "82576";
+    private static String numberAcroExplanation = "Intel 82576 Gigabit Ethernet Controller";
     // Acro has starts with a lowercase char
-    String lowerCaseAcro = "dNAT";
+    private static String lowerCaseAcro = "dNAT";
+    private static String lowerCaseAcroExplanation = "Dynamic Network Address Translation @networking";
     // Acro contains special chars
-    String specialCharAcro = "SMI-S";
+    private static String specialCharAcro = "SMI-S";
+    private static String specialCharAcroExplanation = "Storage Management Initiative Specification";
 
     @Test
     public void testCornerCases() {
-        Assertions.assertThat(dal.getAcronymsByName(numberAcro)).hasSize(1);
-        Assertions.assertThat(dal.getAcronymsByName(lowerCaseAcro)).hasSize(1);
-        Assertions.assertThat(dal.getAcronymsByName(specialCharAcro)).hasSize(1);
+        Assertions.assertThat(dal.getAcronymsByName(numberAcro))
+                .hasSize(1)
+                .filteredOn(acronym -> acronym.getExplanations().contains(numberAcroExplanation));
+        Assertions.assertThat(dal.getAcronymsByName(lowerCaseAcro))
+                .hasSize(1)
+                .filteredOn(acronym -> acronym.getExplanations().contains(lowerCaseAcroExplanation));
+        Assertions.assertThat(dal.getAcronymsByName(specialCharAcro))
+                .hasSize(1)
+                .filteredOn(acronym -> acronym.getExplanations().contains(specialCharAcroExplanation));
         Assertions.assertThat(dal.getAcronymsByName("NON_EXISTENT")).isEmpty();
     }
 }
